df53a62b4ba8cbf73b8ed7f04d1ddcd02ce42a85
omeinsotelo <omeinsotelo@gmail.com>
2020-07-21 18:03:33 -0500

    adding comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,17 +7,17 @@
 def canUnlockAll(boxes):
     """ Method that determines if all the boxes can be opened """
     if boxes:
+        # box is the varible to store in a list the values in boxes
         box = [0]
-        """
-        box = [0]
+        # I go through the list
         for i in box:
+            # I nest boxes[i] to bring the values ​​from each list
             for j in boxes[i]:
+                # I validate the values ​​that I am going to add to my list
                 if j < len(boxes) and j not in box:
-        """
-        for i in range (len(boxes)):
-            for j in boxes[i]:
-                if i != j and j not in box:
+                    # I add the values to my list
                     box.append(j)
+        # I compare the length of the lists
         if len(box) == len(boxes):
             return True
         return False
