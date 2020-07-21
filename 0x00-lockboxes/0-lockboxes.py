a22d32b63c13cc55f2b6e46d33c12bb9964eeb5a
omeinsotelo <omeinsotelo@gmail.com>
2020-07-21 17:21:16 -0500

    upload task

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+    Determines if all the boxes can be opened
+"""
+
+
+def canUnlockAll(boxes):
+    """ Method that determines if all the boxes can be opened """
+    if boxes:
+        box = [0]
+        for i in box:
+            for j in boxes[i]:
+                if j not in box and j != i:
+                    box.append(j)
+        if len(box) == len(boxes):
+            return True
+        return False
