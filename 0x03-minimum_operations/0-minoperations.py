bf96607c11e952cb1b30d9bf9d4b390d8180b963
omeinsotelo <omeinsotelo@gmail.com>
2020-08-10 16:47:33 -0500

    upload task 0

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,14 +4,19 @@
 """
 
 
+def min_divider(n):
+    """ Method that determines the min divider of n int """
+    for i in range(2, n+1):
+        if n % i == 0:
+            return i
+
+
 def minOperations(n):
     """ Method that determines the fewest number of operations """
-    minDiv = 0
-    while isinstance(n, int) and n > 1:
-        div = []
-        for i in range(1, n + 1):
-            if n % i == 0:
-                div.append(i)
-        minDiv += div[1]
-        n = int(n / div[1])
-    return minDiv
+    if not isinstance(n, int):
+        return 0
+    res = 0
+    while n > 1:
+        res += min_divider(n)
+        n = int(n / min_divider(n))
+    return res
