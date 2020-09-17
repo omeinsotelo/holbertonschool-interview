47db9f5c9f9f9d4ae49b4aa302abc99c356892d1
omeinsotelo <omeinsotelo@gmail.com>
2020-09-16 20:35:07 -0500

    upload files

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,9 +1,30 @@
-#ifndef PALINDROME_H
-#define PALINDROME_H
+#include "palindrome.h"
+/**
+ * is_palindrome - define if a number is a palindrome
+ *
+ * @n: number
+ *
+ * Return: 1if it is  or 0 if not
+ */
+int is_palindrome(unsigned long n)
+{
+	int cont1 = 0;
+	int cont2 = 0;
+	unsigned long array[1024];
+	unsigned long newNumber = n;
 
-#include <stdio.h>
-#include <stdlib.h>
+	while (newNumber != 0)
+	{
+		array[cont1++] = newNumber % 10;
+		newNumber = newNumber / 10;
+	}
 
-int is_palindrome(unsigned long n);
+	cont1--;
+	for ( ; cont1 > cont2; cont1--, cont2++)
+	{
+		if (array[cont1] != array[cont2])
+		return (0);
+	}
 
-#endif /* PALINDROME_H */
+	return (1);
+}
--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -1,30 +1,9 @@
-#include "palindrome.h"
-/**
- * is_palindrome - define if a number is a palindrome
- *
- * @n: number
- *
- * Return: 1if it is  or 0 if not
- */
-int is_palindrome(unsigned long n)
-{
-	int cont1 = 0;
-    int cont2 = 0;
-	unsigned long array[1024];
-	unsigned long newNum = n;
+#ifndef PALINDROME_H
+#define PALINDROME_H
 
-	while (newNum != 0)
-	{
-		array[cont1++] = newNum % 10;
-		newNum = newNum / 10;
-	}
+#include <stdio.h>
+#include <stdlib.h>
 
-	cont1--;
-	for ( ; cont1 > cont2; cont1--, cont2++)
-	{
-		if (array[cont1] != array[cont2])
-			return (0);
-	}
+int is_palindrome(unsigned long n);
 
-	return (1);
-}
+#endif /* PALINDROME_H */
