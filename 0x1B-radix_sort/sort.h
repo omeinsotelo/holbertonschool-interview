da61f49468cae79e7dc212a8fccd65b2ede94517
omeinsotelo <omeinsotelo@gmail.com>
2021-03-07 21:41:20 -0500

    Upload files

---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+void print_array(const int *array, size_t size);
+void radix_sort(int *array, size_t size);
+
+#endif /* SORT_H */
+
